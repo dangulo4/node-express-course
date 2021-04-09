206efaa8d06d211326e5ac1074591d9395ccfa29
Daniel Angulo <dangulo4@gmail.com>
2021-04-08 19:50:49 -0700

    adding more fundemental examples

---
+++ 16-stream.js
@@ -0,0 +1,19 @@
+const { createReadStream } = require('fs');
+
+// const stream = createReadStream('./content/big.txt', {
+//   highWaterMark: 9000,
+// });
+const stream = createReadStream('./content/big.txt', {
+  encoding: 'utf8',
+});
+// const stream = createReadStream('./content/big.txt');
+
+// default 64kb
+// last buffer - remainder
+
+stream.on('data', (result) => {
+  console.log(result);
+});
+stream.on('error', (err) => {
+  console.log(err);
+});
